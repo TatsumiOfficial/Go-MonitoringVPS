a0ffa79ea4e217393646f6efb14ae5d6cd1edd58
Tatsumi-Crew Official <71838907+TatsumiOfficial@users.noreply.github.com>
2023-01-04 07:45:00 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,45 @@
+const { exec } = require('child_process');
+const TelegramBot = require('node-telegram-bot-api');
+
+const token = 'PASTEHERE';
+const chatId = 'PASTEHERE';
+
+const bot = new TelegramBot(token, {polling: true});
+
+function getUsage() {
+  return new Promise((resolve, reject) => {
+    exec('top -bn1', (error, stdout) => {
+      if (error) {
+        reject(error);
+      } else {
+        const lines = stdout.split('\n');
+        const cpuLine = lines[2];
+        const cpuUsage = cpuLine.match(/\d+\.\d+/); 
+        exec('free -m', (error, stdout) => {
+          if (error) {
+            reject(error);
+          } else {
+            const lines = stdout.split('\n');
+            const ramLine = lines[1];
+            const ramUsage = ramLine.split(/\s+/)[2]; 
+            const ramFree = ramLine.split(/\s+/)[3]; 
+            resolve({
+              cpu: cpuUsage[0],
+              ram: {
+                used: ramUsage,
+                free: ramFree
+              }
+            });
+          }
+        });
+      }
+    });
+  });
+}
+
+setInterval(() => {
+  getUsage().then((usage) => {
+    const message = `Penggunaan CPU saat ini: ${usage.cpu}%\nPenggunaan RAM saat ini: ${usage.ram.used} MB\nRAM yang tersedia: ${usage.ram.free} MB`;
+    bot.sendMessage(chatId, message);
+  });
+}, 75000); 
